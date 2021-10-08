fc8e8b9e1d9b8dcf04a23547e2e3cb946952bce4
buzhidaoshenme <1773325356@qq.com>
2021-10-08 20:46:39 +0800

    delete

--- custom tools/minAreaRect_func.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import numpy as np
-cnt = np.array([[20, 20], [20, 40], [40, 20], [40, 40]])
-cnt = np.array([[20, 20], [20, 80], [21, 20], [21, 80]])
-cnt = np.array([[20, 20], [20, 21], [40, 20], [40, 21]])
-
-rect = cv2.minAreaRect(cnt)
-print(rect)
