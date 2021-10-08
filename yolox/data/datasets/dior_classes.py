40f4ba728847212a44f7ddadee41d48b90b2b192
buzhidaoshenme <1773325356@qq.com>
2021-10-08 21:00:17 +0800

    re-upload yolox\data\datasets

---
+++ yolox/data/datasets/dior_classes.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Copyright (c) Megvii, Inc. and its affiliates.
+
+# VOC_CLASSES = ( '__background__', # always index 0
+VOC_CLASSES = (
+    'airplane',
+    'airport',
+    'baseballfield',
+    'basketballcourt',
+    'bridge',
+    'chimney',
+    'dam',
+    'Expressway-Service-area',
+    'Expressway-toll-station',
+    'golffield',
+    'groundtrackfield',
+    'harbor',
+    'overpass',
+    'ship',
+    'stadium',
+    'storagetank',
+    'tenniscourt',
+    'trainstation',
+    'vehicle',
+    'windmill',)
